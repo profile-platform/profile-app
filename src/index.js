ed172a7c1638345a570f2bc60ed9aded46f7b5a3
Jesse Sibley <jessesibley@protonmail.com>
2018-05-04 18:44:03 +0100

    Commit for slack. Not working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// Profile App
+// For testing use:
+// http://localhost:1234/?profile=winegum.netlify.com
+
 import { h, app } from 'hyperapp'
 import { Link, Route, location } from '@hyperapp/router'
-import { Profile } from './components/profile'
+import { Picture } from './components/picture'
+import { Posts, Post } from './components/post'
 import './css/main.css'
 
 export function getProfileURL() {
@@ -11,7 +16,6 @@ export function getProfileURL() {
 
     if (searches.indexOf('profile') != -1) {
         const address = searches[searches.indexOf('profile') + 1]
-    
         if (address.endsWith('/')) {
             return 'http://' + address + 'profile.json'
         } else {
@@ -37,14 +41,54 @@ const actions = {
     }
 }
 
-const ProfilePage = async ({ match }) => (
-    <Profile cache={state.profile} />
+const Header = ({ name, nickname, bio, interests }) => (
+    <div class='profile-header'>
+        <Picture />
+
+        <div>
+            <h2 class='profile-header-name'>{ name }</h2>
+            <h3 class='profile-header-nickname'>({ nickname })</h3>
+        
+            <div class='profile-header-bio'>
+                <p>
+                    { bio }
+                </p>
+            </div>
+
+            <div class='profile-header-interests'>
+                <ul>
+                    {interests.map((i) => (
+                        <li>{ i }</li>
+                    ))}
+                </ul>
+            </div>
+
+        </div>
+    </div>
+)
+
+const Footer = state => (
+    <footer class='profile-footer'>
+        Powered by <a href='https://github.com/profile-platform/profile'>Profile</a>
+    </footer>
 )
 
 const view = (state, actions) => {
+    let { name, nickname, bio, interests } = state.profile;
+
     return (
         <div oncreate={() => { actions.fetchProfile() }} >
-            <Route path='/profile' render={ProfilePage} />
+            <div class='container'>
+                <Header
+                    name={name}
+                    nickname={nickname}
+                    bio={bio}
+                    interests={interests}
+                />
+                <Posts />
+                <Footer />
+            </div>
+
         </div>
     )
 }
